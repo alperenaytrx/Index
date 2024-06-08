77e237c08e3d5aaae01aeb07b39cad060aafc8ba
alperenaytrx <166352480+alperenaytrx@users.noreply.github.com>
2024-06-08 14:22:00 +0000

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/1280px-Flag_of_Turkey.svg.png" type="image/x-icon" />
 <title>Alperen Aydemir - Ayyıldız Team Sitenizi Hacklemistir.</title>
 <meta charset="utf8">
 </head>
@@ -9,14 +8,11 @@
 <script>
 alert("Hacked By TURKHACKTEAM");
 </script>
-<center><img src="https://i.ytimg.com/vi/1JN8M1ofxSc/maxresdefault.jpg" alt="" width="" height=""></img></center>
-<center><><font color="#ffffff" family="Tahoma" size="18">Geçmiş Olsun</font></></center>
+  <h1>HACK4D BY ALP3R3NX</h1>
+  <style>
+    h1{
+      color: red;
+    }
+  </style>
 </body>
-<!--Bu index THT İndex Makerla Oluşturulmuştur-->
-<!--
-|_   _|   _ _ __| | _| | | | __ _  ___| | _|_   _|__  __ _ _ __ ___
-  | || | | | '__| |/ / |_| |/ _` |/ __| |/ / | |/ _ \/ _` | '_ ` _   | || |_| | |  |   <|  _  | (_| | (__|   <  | |  __/ (_| | | | | | |
-  |_| \__,_|_|  |_|\_\_| |_|\__,_|\___|_|\_\ |_|\___|\__,_|_| |_| |_|
--->
-
 </html>
